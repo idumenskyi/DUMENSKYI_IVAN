b112397e8318237b742462774f0bfb6f04f2ecc2
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2020-01-18 09:14:33 +0200

    Update PacMan.cpp

--- PacMan.cpp
+++ PacMan.cpp
@@ -1,10 +1,10 @@
+#include <iostream>
+#include <sstream>
+#include "Windows.h"
 #include "PacMan.h"
+namespace myNamespace {
 
-int x = 1;
-int y = 1;
-int score = 0;
-
-char maze[10][30] = {
+    char maze[10][30] = {
     "########        ########",
     "#@*****#        #******#",
     "# *****##########**+***#",
@@ -12,56 +12,120 @@ char maze[10][30] = {
     "#**+***##########***+**#",
     "#******#        #******#",
     "########        ########"
-};
+    };
+
+    int x;
+    int y;
+    static int score;
 
 
-PacMan::PacMan()
-{
-
-}
 
 
-PacMan::~PacMan()
-{
-
-}
-
-void PacMan::Move(int V, int H)
-{
-    int y2 = y + V;
-    int x2 = x + H;
-
-    if (maze[y2][x2] == '+')
+    void PacMan::printMap()
     {
-        maze[y2 - 1][x2 - 1] = '*';
-        maze[y2 - 1][x2] = '*';
-        maze[y2 - 1][x2 + 1] = '*';
-        maze[y2][x2 - 1] = '*';
-        maze[y2][x2 + 1] = '*';
-        maze[y2 + 1][x2 - 1] = '*';
-        maze[y2 + 1][x2] = '*';
-        maze[y2 + 1][x2 + 1] = '*';
-    }
-    if (maze[y2][x2] == '*')
-    {
-        score++;
-    }
-    if (maze[y][x2] == ' '
-        || maze[y][x2] == '*'
-        || maze[y][x2] == '+')
-    {
-        maze[y][x] = ' ';
-        x += H;
-        maze[y][x] = '@';
+        for (int i = 0; i < 10; i++)
+        {
+            std::cout << maze[i] << std::endl;
+        }
 
     }
-    if (maze[y2][x] == ' '
-        || maze[y2][x] == '*'
-        || maze[y2][x] == '+')
+
+    void PacMan::Move(int V, int H)
     {
-        maze[y][x] = ' ';
-        y += V;
-        maze[y][x] = '@';
+        int y2 = y + V;
+        int x2 = x + H;
+
+        if (maze[y2][x2] == '+')
+        {
+            maze[y2 - 1][x2 - 1] = '*';
+            maze[y2 - 1][x2] = '*';
+            maze[y2 - 1][x2 + 1] = '*';
+            maze[y2][x2 - 1] = '*';
+            maze[y2][x2 + 1] = '*';
+            maze[y2 + 1][x2 - 1] = '*';
+            maze[y2 + 1][x2] = '*';
+            maze[y2 + 1][x2 + 1] = '*';
+        }
+        if (maze[y2][x2] == '*')
+        {
+            score++;
+        }
+        if (maze[y][x2] == ' '
+            || maze[y][x2] == '*'
+            || maze[y][x2] == '+')
+        {
+            maze[y][x] = ' ';
+            x += H;
+            maze[y][x] = '@';
+
+        }
+        if (maze[y2][x] == ' '
+            || maze[y2][x] == '*'
+            || maze[y2][x] == '+')
+        {
+            maze[y][x] = ' ';
+            y += V;
+            maze[y][x] = '@';
+
+        }
+
 
     }
+    void PacMan::points()
+    {
+        std::stringstream ss;
+        ss << score;
+        std::string items = "Score: ";
+        items += ss.str();
+        for (int i = 0; i < 1; i++)
+        {
+            std::cout << items << std::endl;
+        }
+    }
+    void PacMan::pressKey()
+    {
+        /*
+        int vKey;
+        
+        switch (vKey)
+        {
+            case GetAsyncKeyState('W'):
+            Move(-1, 0);
+            break;
+            case GetAsyncKeyState('S'):
+            Move(1, 0);
+            break;
+            case GetAsyncKeyState('A'):
+            Move(0, -1);
+            break;
+            case GetAsyncKeyState('D'):
+            Move(0, 1);
+            break;
+            default:
+                break;
+
+        }
+        */
+
+        if (GetAsyncKeyState('W'))
+        {
+            Move(-1, 0);
+        }
+        else if (GetAsyncKeyState('S'))
+        {
+            Move(1, 0);
+        }
+        else if (GetAsyncKeyState('A'))
+        {
+            Move(0, -1);
+        }
+        else if (GetAsyncKeyState('D'))
+        {
+            Move(0, 1);
+        }
+
+    }
+
+
+
 }
